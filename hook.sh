07708e7175ea292d33d1bd42e4479576689d0abf
Matt M <mattm@schooldata.net>
2022-03-19 22:16:53 -0500

    finish modularization

--- hook.sh
+++ hook.sh
@@ -29,9 +29,9 @@ find-awesome-dbus-addresses() {
 }
 
 send() {
-  if [[ -z $LAST_AWESOME_ADDRESS ]]; then
+#  if [[ -z $LAST_AWESOME_ADDRESS ]]; then
     LAST_AWESOME_ADDRESS=$(find-awesome-dbus-addresses | sort -t'.' -k 1.1nbr -k 2.1nbr | head -n 1 | tr -d '\n')
-  fi
+#  fi
   if [[ -n $1 ]]; then
     STDIN=$1
   else
---
+++ main/tags.lua
@@ -0,0 +1,41 @@
+-- Standard awesome library
+local awful = require("awful")
+local sharedtags = require("awesome-sharedtags")
+local workspaceManager = require("awesome-workspace-manager")
+
+local _M = {}
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+function _M.get ()
+
+
+    local tags1 = sharedtags({
+        { name = "tag in workspace1", layout = awful.layout.layouts[2] },
+        { name = "movies", layout = awful.layout.layouts[10] },
+    })
+
+    local tags2 = sharedtags({
+        { name = "tag in workspace2", layout = awful.layout.layouts[2] },
+        { name = "music", layout = awful.layout.layouts[10] },
+    })
+
+    --__.forEach(tags1, function(o)  o.activated = false end)
+    --__.forEach(tags2, function(o)  o.activated = false end)
+
+    wm = workspaceManager:new()
+
+    wm:createWorkspace('workspace_1', tags1)
+    wm:createWorkspace('workspace_2', tags2)
+    --wm:switchTo(1)
+
+    return nil
+end
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+return setmetatable({}, { __call = function(_, ...) return _M.get(...) end })
+
+-- {{{ Tags
+
+-- }}}
