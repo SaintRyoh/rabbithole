7f8f142a449313781155adb7899e5fa4f77341a7
LycanDarko <lycandarko@outlook.com>
2023-04-05 22:19:46 -0700

    taglist widget test

--- awesome-workspace-manager/widgets/taglist/template.lua
+++ awesome-workspace-manager/widgets/taglist/template.lua
@@ -4,67 +4,66 @@ local awful = require("awful")
 local naughty = require("naughty")
 local gears = require("gears")
 local __ = require("lodash")
+local color = require("src.themes.rabbithole.colors")
+
 
 local _M = {}
 
 function _M.get(controller)
-        return {
+    return     {
+        {
             {
                 {
                     {
                         {
-                            {
-                                id     = 'index_role',
-                                widget = wibox.widget.textbox,
-                            },
-                            margins = 4,
-                            widget  = wibox.container.margin,
+                            id     = 'index_role',
+                            widget = wibox.widget.textbox,
                         },
-                        bg     = '#dddddd',
-                        shape  = gears.shape.circle,
-                        widget = wibox.container.background,
-                    },
-                    {
-                        {
-                            id     = 'icon_role',
-                            widget = wibox.widget.imagebox,
-                        },
-                        margins = 2,
+                        margins = 4,
                         widget  = wibox.container.margin,
                     },
-                    {
-                        id     = 'text_role',
-                        widget = wibox.widget.textbox,
-                    },
-                    layout = wibox.layout.fixed.horizontal,
+                    bg     = '#dddddd',
+                    shape  = gears.shape.circle,
+                    widget = wibox.container.background,
                 },
-                left  = 18,
-                right = 18,
-                widget = wibox.container.margin
+                {
+                    {
+                        id     = 'icon_role',
+                        widget = wibox.widget.imagebox,
+                    },
+                    margins = 2,
+                    widget  = wibox.container.margin,
+                },
+                {
+                    id     = 'text_role',
+                    widget = wibox.widget.textbox,
+                },
+                layout = wibox.layout.fixed.horizontal,
             },
-            id     = 'background_role',
-            widget = wibox.container.background,
-            -- Add support for hover colors and an index label
-            create_callback = function(self, c3, index, objects) --luacheck: no unused args
-                self:get_children_by_id('index_role')[1].markup = '<b> '..index..' </b>'
-                self:connect_signal('mouse::enter', function()
-                    if self.bg ~= '#ff0000' then
-                        self.backup     = self.bg
-                        self.has_backup = true
-                    end
-                    self.bg = '#ff0000'
-                end)
-                self:connect_signal('mouse::leave', function()
-                    if self.has_backup then self.bg = self.backup end
-                end)
-            end,
-            update_callback = function(self, c3, index, objects) --luacheck: no unused args
-                self:get_children_by_id('index_role')[1].markup = '<b> '..index..' </b>'
-            end,
-        }
-
+            left  = 18,
+            right = 18,
+            widget = wibox.container.margin
+        },
+        id     = 'background_role',
+        widget = wibox.container.background,
+        -- Add support for hover colors and an index label
+        create_callback = function(self, c3, index, objects) --luacheck: no unused args
+            self:get_children_by_id('index_role')[1].markup = '<b> '..index..' </b>'
+            self:connect_signal('mouse::enter', function()
+                if self.bg ~= '#ff0000' then
+                    self.backup     = self.bg
+                    self.has_backup = true
+                end
+                self.bg = '#ff0000'
+            end)
+            self:connect_signal('mouse::leave', function()
+                if self.has_backup then self.bg = self.backup end
+            end)
+        end,
+        update_callback = function(self, c3, index, objects) --luacheck: no unused args
+            self:get_children_by_id('index_role')[1].markup = '<b> '..index..' </b>'
+        end,
+    }
 end
-
-
 
 return setmetatable({}, { __call = function(_, controller) return _M.get(controller) end })
