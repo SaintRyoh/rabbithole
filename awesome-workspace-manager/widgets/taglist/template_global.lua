7d3a07867d259ac815132eaf8d79110de481accf
LycanDarko <lycandarko@outlook.com>
2023-04-12 23:43:23 -0700

    reverted taglist_local template to dev branch version,
    svg backgrounds for items seem to cause disappearing bugs for now. We will have to figure out why later, its just getting in the way

--- awesome-workspace-manager/widgets/taglist/template_global.lua
+++ awesome-workspace-manager/widgets/taglist/template_global.lua
@@ -1,44 +1,24 @@
 local wibox = require("wibox")
-local gears = require("gears")
-local awful = require("awful")
 
-local config_dir = awful.util.getdir("config")
-local glass_texture_path = config_dir .. "/src/assets/textures/glass_texture.svg"
 
 return function (controller)
-    return
+    return 
     {
         id     = 'background_role',
         widget = wibox.container.background,
-        shape = function(cr, width, height)
-            gears.shape.rounded_rect(cr, width, height, 10)
-        end,
-
         {
-            layout = wibox.layout.stack,
+            left  = 18,
+            right = 18,
+            widget = wibox.container.margin,
             {
-                widget = wibox.container.place,
+                layout = wibox.layout.fixed.horizontal,
                 {
-                    id = 'bg_image',
-                    widget = wibox.widget.imagebox,
-                    image = glass_texture_path,
-                    resize = true,
+                    widget = wibox.container.margin,
+                    left = 5,
                 },
-            },
-            {
-                left  = 18,
-                right = 18,
-                widget = wibox.container.margin,
                 {
-                    layout = wibox.layout.fixed.horizontal,
-                    {
-                        widget = wibox.container.margin,
-                        left = 5,
-                    },
-                    {
-                        id     = 'text_role',
-                        widget = wibox.widget.textbox,
-                    },
+                    id     = 'text_role',
+                    widget = wibox.widget.textbox,
                 },
             },
         },
@@ -46,15 +26,6 @@ return function (controller)
         -- Add support for hover colors and an index label
         create_callback = function (tag_template, tag, index, objects) controller:create_tag_callback(tag_template, tag, index, objects) end,
 
-        update_callback = function (tag_template, tag, index, objects)
-            local bg_image = objects.bg_image
-            local widget_width = tag_template:get_width()
-            local widget_height = tag_template:get_height()
-
-            bg_image.forced_width = widget_width
-            bg_image.forced_height = widget_height
-
-            controller:update_tag_callback(tag_template, tag, index, objects)
-        end,
+        update_callback = function (tag_template, tag, index, objects) controller:update_tag_callback(tag_template, tag, index, objects) end,
     }
 end
