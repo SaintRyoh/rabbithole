47c32382b67c370390dd1142b83b8fe9a3b1427b
SaintRyoh <14931827+SaintRyoh@users.noreply.github.com>
2023-04-20 22:55:27 -0500

    change theme back

--- rc.lua
+++ rc.lua
@@ -24,7 +24,7 @@ RC = {
 
 
         config.bindings.values.settings = {
-            theme_dir = "themes/default/theme.lua",
+            theme_dir = "themes/rabbithole/theme.lua",
             modkey = "Mod4",
             terminal = "qterminal",
             editor = os.getenv("EDITOR") or "nvim",
