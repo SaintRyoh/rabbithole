0f6c5a5edaa448e589f82315d28b07485a8e6215
LycanDarko <lycandarko@outlook.com>
2023-04-26 19:08:47 -0700

    fixed icon widget, still has some theming issues. cosmetically imperfect
    For now.

--- awesome-workspace-manager/widgets/tasklist/view.lua
+++ awesome-workspace-manager/widgets/tasklist/view.lua
@@ -1,42 +1,62 @@
 local wibox = require("wibox")
 local awful = require("awful")
+local gears = require("gears")
 
 local _M = {}
+
 local generate_filter = function(t)
-	return function(c, scr)
-		local ctags = c:tags()
-		for _, v in ipairs(ctags) do
-			if v == t then
-				return true
-			end
-		end
-		return false
-	end
+    return function(c, scr)
+        local ctags = c:tags()
+        for _, v in ipairs(ctags) do
+            if v == t then
+                return true
+            end
+        end
+        return false
+    end
 end
+
 function _M.create(tasklist_buttons, s, tag)
-    -- return wibox.widget.textbox "Tasklist"
     return awful.widget.tasklist({
-		screen = s,
-        -- filter  = awful.widget.tasklist.filter.currenttags,
+        screen = s,
         filter  = generate_filter(tag),
-		buttons = tasklist_buttons,
-		widget_template = {
-			{
-				id = "clienticon",
-				widget = awful.widget.clienticon,
-			},
-			layout = wibox.layout.stack,
-			create_callback = function(self, c, _, _)
-				self:get_children_by_id("clienticon")[1].client = c
-				awful.tooltip({
-					objects = { self },
-					timer_function = function()
-						return c.name
-					end,
-				})
-			end,
-		},
-	})
+        buttons = tasklist_buttons,
+        widget_template = {
+            {
+                {
+                    id = "clienticon",
+                    widget = wibox.widget.imagebox,
+                    forced_width = 24,
+                    forced_height = 24,
+                },
+                id = 'background_role',
+                widget = wibox.container.background,
+                bg = "#00000000",
+                fg = "#00000000",
+            },
+            layout = wibox.layout.stack,
+            create_callback = function(self, c, _, _)
+                if c.icon then
+                    self:get_children_by_id("clienticon")[1].image = c.icon
+                else
+                    self:get_children_by_id("clienticon")[1].image = gears.surface.load_uncached(gears.filesystem.get_configuration_dir() .. "/themes/rabbithole/icons/fallback.svg")
+                end
+
+                self:connect_signal('mouse::enter', function()
+                    self:get_children_by_id('background_role')[1]:set_bg('#BBBBBB22')
+                    self:get_children_by_id('background_role')[1]:set_fg('#BBBBBB22')
+                    self:get_children_by_id('background_role')[1]:set_fg('#00000000')
+                end)
+
+                awful.tooltip({
+                    objects = { self },
+                    timer_function = function()
+                        return c.name
+                    end,
+                })
+            end,
+        },
+    })
 end
 
 return _M
