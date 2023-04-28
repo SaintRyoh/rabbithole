70e4d49d1bc4709876c0128b0f753cf4bc7aa669
LycanDarko <lycandarko@outlook.com>
2023-04-28 01:55:44 -0700

    kinda what we were going for, almost there

--- awesome-workspace-manager/widgets/taglist/template_local.lua
+++ awesome-workspace-manager/widgets/taglist/template_local.lua
@@ -2,49 +2,49 @@ local wibox = require("wibox")
 local gears = require("gears")
 local beautiful = require("beautiful")
 
+local gradient_bg = beautiful.taglist_bg_focus
 
 return function (controller)
     return {
-        -- for some reason, having the container with the id=background role makes it not take a shape
-        --id     = 'background_role',
         widget = wibox.container.background,
         shape = function(cr, width, height)
             gears.shape.rounded_rect(cr, width, height, 10)
         end,
-        -- bugfix to revent tags from inheriting rabid_bars color until it is moused-over
         bg = beautiful.bg_normal,
         {
-            widget = wibox.container.margin,
+            layout = wibox.layout.fixed.horizontal,
             {
-                left  = 5,
-                right = 5,
+                id     = 'index_role',
+                widget = wibox.widget.textbox,
+            },
+            {
                 widget = wibox.container.margin,
+                top = 10, -- Increase top margin here
+                bottom = 10, -- Increase bottom margin here
                 {
-                    layout = wibox.layout.fixed.horizontal,
+                    widget = wibox.container.background,
+                    shape = function(cr, width, height)
+                        gears.shape.rounded_rect(cr, width, height, 10)
+                    end,
+                    bg = gradient_bg,
                     {
-                        id     = 'index_role',
-                        widget = wibox.widget.textbox,
-                    },
-                    {
-                        widget = wibox.container.margin,
-                        left = 5,
-                    },
-                    {
-                        id     = 'text_role',
-                        widget = wibox.widget.textbox,
-                    },
-                    {
-                        id = "icon_container",
-                        widget = wibox.container.place,
+                        layout = wibox.layout.fixed.horizontal,
                         {
-                            widget = wibox.layout.fixed.horizontal,
-                        }
+                            id     = 'text_role',
+                            widget = wibox.widget.textbox,
+                        },
+                        {
+                            id = "icon_container",
+                            widget = wibox.container.place,
+                            {
+                                widget = wibox.layout.fixed.horizontal,
+                            }
+                        },
                     },
                 },
             },
         },
 
-        -- Add support for hover colors and an index label
         create_callback = function (tag_template, tag, index, objects) controller:create_tag_callback(tag_template, tag, index, objects) end,
 
         update_callback = function (tag_template, tag, index, objects) controller:update_tag_callback(tag_template, tag, index, objects) end,
