76bc276d2a3dae73011a8fe1187fec82b4a3a88f
SaintRyoh <14931827+SaintRyoh@users.noreply.github.com>
2023-04-30 16:42:52 -0500

    move ui components to ui folder

--- rabbithole/ui/default/init.lua
+++ rabbithole/ui/default/init.lua
@@ -3,9 +3,9 @@ local awful     = require("awful")
 return setmetatable({}, {
     __constructor = function(
         workspaceManagerService, 
-        rabbithole__components__wiboxes__default___bars__left, 
-        rabbithole__components__wiboxes__default___bars__center, 
-        rabbithole__components__wiboxes__default___bars__right
+        rabbithole__ui__default__left, 
+        rabbithole__ui__default__center, 
+        rabbithole__ui__default__right
     )
         awful.screen.connect_for_each_screen(function(s)
 
@@ -14,9 +14,9 @@ return setmetatable({}, {
             --     workspaceManagerService.session_restored = true
             -- end
 
-            rabbithole__components__wiboxes__default___bars__left(s)
-            rabbithole__components__wiboxes__default___bars__center(s)
-            rabbithole__components__wiboxes__default___bars__right(s)
+            rabbithole__ui__default__left(s)
+            rabbithole__ui__default__center(s)
+            rabbithole__ui__default__right(s)
 
 
         end)
