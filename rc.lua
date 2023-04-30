d22d0f06df166fdcf335012a689e33920def6dd9
SaintRyoh <14931827+SaintRyoh@users.noreply.github.com>
2023-04-30 14:23:28 -0500

    move theme to systems

--- rc.lua
+++ rc.lua
@@ -19,7 +19,7 @@ RC = {
 
 
         -- Make theme a singleton (so we only call beautiful.init once)
-        config.bindings.types.theme = "rabbithole.services.theme-loader"
+        config.bindings.types.theme = "rabbithole.systems.theme-loader"
         config.singletons.theme = true
 
 
