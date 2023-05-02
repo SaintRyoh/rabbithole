69e3e46d94e567e9c6cb3255520e53bb47773fe6
SaintRyoh <14931827+SaintRyoh@users.noreply.github.com>
2023-05-01 22:40:58 -0500

    only open tag preview once

--- rabbithole/services/tag-preview.lua
+++ rabbithole/services/tag-preview.lua
@@ -1,5 +1,8 @@
+local gears = require("gears")
 local Module = {}
 Module.__index = Module
+
+local visible = false
 
 function Module.new()
     local self = {}
@@ -10,17 +13,21 @@ end
 
 function Module.show(tag, screen)
     -- BLING: Only show widget when there are clients in the tag
-    if #tag:clients() > 0 then
+    if #tag:clients() > 0 and not visible then
         -- BLING: Update the widget with the new tag
         awesome.emit_signal("bling::tag_preview::update", tag)
         -- BLING: Show the widget
         awesome.emit_signal("bling::tag_preview::visibility", screen, true)
+        visible = true
     end
 end
 
 function Module.hide(screen)
     -- BLING: Turn the widget off
-    awesome.emit_signal("bling::tag_preview::visibility", screen, false)
+    if visible then
+        awesome.emit_signal("bling::tag_preview::visibility", screen, false)
+        visible = false
+    end
 end
 
 return Module
