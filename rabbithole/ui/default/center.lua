242ad79101ae5da9b93b8aecc501c75a1aa19fed
SaintRyoh <14931827+SaintRyoh@users.noreply.github.com>
2023-05-07 03:11:25 -0500

    convert layout widget to use DI

--- rabbithole/ui/default/center.lua
+++ rabbithole/ui/default/center.lua
@@ -4,7 +4,8 @@ local layoutlist_widget = require("rabbithole.components.widgets.layout_list")
 return setmetatable({}, {
     __constructor = function(
         rabbithole__components__widgets__taglist, 
-        rabbithole__components__wiboxes__bars__center
+        rabbithole__components__wiboxes__bars__center,
+        rabbithole__components__widgets__layout_list
     )
         return function(s)
             local custom_layoutlist = layoutlist_widget.new(s)
@@ -12,7 +13,7 @@ return setmetatable({}, {
             rabbithole__components__wiboxes__bars__center(s):setup {
                 layout = wibox.layout.align.horizontal,
                 rabbithole__components__widgets__taglist(s),
-                custom_layoutlist,
+                rabbithole__components__widgets__layout_list(s),
             }
         end
     end
