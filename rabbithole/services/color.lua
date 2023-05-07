0de03c0adea8b985ff8f9f0f059c5f1c41d5023a
LycanDarko <lycandarko@outlook.com>
2023-05-06 18:11:56 -0700

    added hsl to rgb function to colors.lua

--- rabbithole/services/color.lua
+++ rabbithole/services/color.lua
@@ -7,10 +7,10 @@ local max, min, pow, floor, random = math.max, math.min, math.pow, math.floor, m
 --[[ This is the color service for tesseract. Capable of manipulating colors
 in almost every conceivable way.
 ]]
+
 local ColorService = color
 ColorService.__index = ColorService
 
--- Constructor for ColorService
 function ColorService.new()
     local self = {}
     setmetatable(self, ColorService)
@@ -179,6 +179,32 @@ function ColorService:hsv2hex(H, S, V)
     local r, g, b = (r_ + m) * 255, (g_ + m) * 255, (b_ + m) * 255
     return ("#%02x%02x%02x"):format(floor(r), floor(g), floor(b))
 end
+
+local function ColorServices:hsl_to_rgb(h, s, l)
+    local r, g, b
+    if s == 0 then
+        r, g, b = l, l, l
+    else
+        local hue_to_rgb = function(p, q, t)
+            if t < 0 then t = t + 1 end
+            if t > 1 then t = t - 1 end
+            if t < 1/6 then return p + (q - p) * 6 * t end
+            if t < 1/2 then return q end
+            if t < 2/3 then return p + (q - p) * (2/3 - t) * 6 end
+            return p
+        end
+
+        local q = l < 0.5 and l * (1 + s) or l + s - l * s
+        local p = 2 * l - q
+
+        r = hue_to_rgb(p, q, h + 1/3)
+        g = hue_to_rgb(p, q, h)
+        b = hue_to_rgb(p, q, h - 1/3)
+    end
+
+    return r * 255, g * 255, b * 255
+end
+
 -- END CONVERSIONS ]]]
 
 -- [[[ CONSTRAST - These functions calculate/generate contasts
