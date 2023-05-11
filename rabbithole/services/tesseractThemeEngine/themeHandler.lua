02dafe49e7d3d128866e99634f7a082c5b5dae32
LycanDarko <lycandarko@outlook.com>
2023-05-11 01:40:02 -0700

    cleaning up tesseract

--- rabbithole/services/tesseractThemeEngine/themeHandler.lua
+++ rabbithole/services/tesseractThemeEngine/themeHandler.lua
@@ -1,18 +1,18 @@
 local http = require("socket.http")
 local chromaticTesseract = require("chromaticTesseract")
-local beautiful = require("beautiful")
+local naughty = require("naughty")
 
 --[[ Usage:
-local themeHandler = require("themeHandler")
+local Tesseract = require("Tesseract")
 
 -- Use a GitHub URL to fetch the theme. This will return the theme table
-local theme_table = themeHandler.generate_theme("https://raw.githubusercontent.com/user/repo/branch/theme_file.lua", "github")
+local theme_table = Tesseract:generate_theme("https://raw.githubusercontent.com/user/repo/branch/theme_file.lua")
 
 -- Use a local file to fetch the theme. This will return the theme table
-local theme_table = themeHandler.generate_theme("/path/to/your/theme/file.lua", "local")
+local theme_table = Tesseract:generate_theme("/path/to/your/theme/file.lua")
 
 -- Use chromaticTesseract to generate theme. This will return the theme table
-local theme_table = themeHandler.generate_theme(nil, nil, "#4CAF50", "complementary", {
+local theme_table = Tesseract:generate_theme(nil, "#4CAF50", "complementary", {
     saturation_range = 0.1,
     lightness_range = 0.1,
     font = "Roboto 12",
@@ -27,10 +27,11 @@ local theme_table = themeHandler.generate_theme(nil, nil, "#4CAF50", "complement
 Then apply theme with beautiful.init(theme_table)
 ]]
 
-local themeHandler = {}
+local Tesseract = {}
+Tesseract.__index = Tesseract
 
 -- Fetch theme content from a GitHub URL
-local function fetch_theme_from_github(url)
+function Tesseract:fetch_theme_from_github(url)
     local content = http.request(url)
     if content then
         return loadstring(content)()
@@ -41,7 +42,7 @@ local function fetch_theme_from_github(url)
 end
 
 -- Read theme content from a local file
-local function read_theme_from_file(file_path)
+function Tesseract:read_theme_from_file(file_path)
     local file, err = io.open(file_path, "r")
     if not file then
         naughty.notify({title = "Error", text = "Failed to read theme from local file: " .. err})
@@ -54,23 +55,25 @@ end
 
 -- Main function to handle theme fetching
 -- @This function can handle any of theme types and detect it automatically.
-function themeHandler.generate_theme(theme_source, source_type, primary_color, color_scheme, options)
-    local theme_table = nil
-
-    if primary_color and color_scheme then
-        theme_table = chromaticTesseract.generate_theme(primary_color, color_scheme, options)
-    else
-        if source_type == "github" then
-            theme_table = fetch_theme_from_github(theme_source)
-        elseif source_type == "local" then
-            theme_table = read_theme_from_file(theme_source)
+    function Tesseract:generate_theme(theme_source, primary_color, color_scheme, options)
+        local theme_table = nil
+    
+        -- Case: Generate theme from chromaticTesseract
+        if primary_color and color_scheme then
+            theme_table = chromaticTesseract.generate_theme(primary_color, color_scheme, options)
+        -- Case: Generate theme from GitHub URL
+        elseif type(theme_source) == "string" and string.match(theme_source, "^https://") then
+            theme_table = self:fetch_theme_from_github(theme_source)
+        -- Case: Generate theme from local file
+        elseif type(theme_source) == "string" then
+            theme_table = self:read_theme_from_file(theme_source)
         else
-            naughty.notify({title = "Error", text = "Invalid theme source type."})
+            naughty.notify({title = "Error", text = "Invalid theme source."})
             return
         end
+    
+        return theme_table
     end
+    
 
-    return theme_table
-end
-
-return themeHandler
+return Tesseract
