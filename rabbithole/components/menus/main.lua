bcf9fc015c69f27e79918d2b241cacb39cc943ad
LycanDarko <lycandarko@outlook.com>
2023-05-14 12:51:06 -0700

    reverted back to freedesktop sub

--- rabbithole/components/menus/main.lua
+++ rabbithole/components/menus/main.lua
@@ -1,11 +1,9 @@
 -- Standard awesome library
 local awful = require("awful")
-local gears = require("gears")
 local hotkeys_popup = require("awful.hotkeys_popup").widget
 -- Theme handling library
 local beautiful = require("beautiful") -- for rabbithole icon
 local freedesktop = require("sub.freedesktop")
-local apps = require("rabbithole.services.appService")
 
 
 return setmetatable({}, {
@@ -20,7 +18,7 @@ return setmetatable({}, {
                 hotkeys_popup.show_help(nil, awful.screen.focused())
             end },
             --{ "AWM Manual", terminal .. " -e man awesome" },
-            { "Launch terminal", terminal },
+            { "Launch term", terminal },
             { "Logout", function() awesome.quit() end },
             { "Restart WM", awesome.restart },
             { "Quit Rabbithole", function() awesome.quit() end },
@@ -40,12 +38,12 @@ return setmetatable({}, {
             { "Hotspot", "wihotspot-gui" }
         }
         -- Create an applications submenu from freedesktop
-        M.applications = apps.get_apps()
+        M.applications = freedesktop.menu.build()
         -- Main Menu
         local menu_items = {
             { "Rabbithole", M.rabbithole, beautiful.rabbit_menu},
-            { "Terminal", terminal },
-            --{ "WiFi", M.network_main },
+            { "Launch terminal", terminal },
+            { "WiFi", M.network_main },
             { "Applications", M.applications }
             --{ "favorite", M.favorite }
         }
