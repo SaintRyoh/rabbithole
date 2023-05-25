71f8fb52f2552a4d8ac08285fd2947ee994a16dc
LycanDarko <lycandarko@outlook.com>
2023-05-25 15:43:14 -0700

    made nice work with theme, changed icon order

--- rabbithole/systems/theme-loader.lua
+++ rabbithole/systems/theme-loader.lua
@@ -14,7 +14,28 @@ return setmetatable({}, {
 
         if theme_table then
             beautiful.init(theme_table)
-            nice()
+            nice{
+                titlebar_height =  35,
+                titlebar_radius = 13,
+                titlebar_font = beautiful.font,
+                no_titlebar_maximized = true,
+                titlebar_items = {
+                    left = {"floating", "ontop", "sticky"},
+                    middle = "title",
+                    right = {"minimize", "maximize", "close"},
+                },
+                context_menu_theme = {
+                    bg_focus = beautiful.bg_focus,
+                    bg_normal = beautiful.bg_normal,
+                    border_color = beautiful.border_focus,
+                    border_width = 3,
+                    fg_focus =  beautiful.fg_focus,
+                    fg_normal = beautiful.fg_normal,
+                    font = beautiful.font,
+                    height = 27.5,
+                    width = 250,
+                }
+            }
         else
             naughty.notify({title = "Error", text = "Failed to generate theme."})
         end
