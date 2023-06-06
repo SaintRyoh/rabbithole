78360732b71f043f6667ab1f5c777a624b1548e3
LycanDarko <lycandarko@outlook.com>
2023-06-06 14:26:35 -0700

    Bare bones settings manager working

--- rabbithole/services/settingsManager/init.lua
+++ rabbithole/services/settingsManager/init.lua
@@ -1,6 +1,6 @@
 local wibox = require("wibox")
-local awful = require("awful")
 local gears = require("gears")
+local dir = require("pl.dir")
 
 local SettingsManager = {}
 SettingsManager.__index = SettingsManager
@@ -9,6 +9,9 @@ function SettingsManager.new(settings)
     local self = setmetatable({}, SettingsManager)
 
     self.settings = settings or {}
+    -- settings manager directory
+    self.directory = gears.filesystem.get_configuration_dir() .. "rabbithole/services/settingsManager/"
+    self.settings_file = gears.filesystem.get_configuration_dir() .. "settings.lua"
 
     self.window = wibox({
         width = 800,
@@ -20,24 +23,46 @@ function SettingsManager.new(settings)
     self.layout = wibox.layout.fixed.vertical()
     self.window:set_widget(self.layout)
 
+    -- Add a "titlebar" to the wibox
+    local titlebar = wibox.widget.textbox("Settings Manager")
+    titlebar.font = "Sans Bold 14"
+    titlebar.align = "center"
+    self.layout:add(titlebar)
+
     self.tabs = {}
 
-    for category, values in pairs(self.settings) do
-        local tabButton = wibox.widget.textbox(category)
-        tabButton:connect_signal("button::press", function()
-            self:showTab(category)
-        end)
-        self.layout:add(tabButton)
-        self.tabs[category] = values
+    -- Dynamically create tabs based on the modules in the "settingsManager" directory
+    local modules = dir.getallfiles(self.directory)
+    for _, module in ipairs(modules) do
+        if module:sub(-4) == ".lua" then
+            local filePath = module:sub(self.directory:len() + 1, -1)
+            local moduleName = filePath:sub(1, -5)
+            local tabButton = wibox.widget.textbox(moduleName)
+            tabButton:connect_signal("button::press", function()
+                self:showTab(moduleName)
+            end)
+            self.layout:add(tabButton)
+
+            -- Load the module and save its content
+            local moduleContent = require("rabbithole.services.settingsManager." .. moduleName)
+            self.tabs[moduleName] = moduleContent
+        end
     end
 
     return self
 end
 
-function SettingsManager:showTab(category)
-    local settings = self.tabs[category]
+function SettingsManager:showTab(moduleName)
+    local settings = self.tabs[moduleName]
     if settings then
         self.layout:reset()
+
+        -- Add the "titlebar" again after reset
+        local titlebar = wibox.widget.textbox("Settings Manager - " .. moduleName)
+        titlebar.font = "Sans Bold 14"
+        titlebar.align = "center"
+        self.layout:add(titlebar)
+
         for key, value in pairs(settings) do
             local row = wibox.widget.textbox(key .. ": " .. tostring(value))
             self.layout:add(row)
