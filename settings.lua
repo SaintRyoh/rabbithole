76564e6d20a47ef877e9849a2fdc5651e565eacf
LycanDarko <lycandarko@outlook.com>
2023-06-07 13:12:32 -0700

    Created a settings.lua file in root dir

---
+++ settings.lua
@@ -0,0 +1,14 @@
+--[[ Settings file for Rabbithole
+
+Creates a settings object to be used with the settingsManager service.
+]]
+
+local Settings = { }
+Settings.__index = Settings
+
+function Settings.new(settings)
+    local self = setmetatable({ }, Settings)
+    -- TODO:
+    -- Define settings variables and their defaults
+
+end
