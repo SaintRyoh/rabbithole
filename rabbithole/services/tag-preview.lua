cdc839973d2bef7fddbf1fceca3bf2ba413c3a01
LycanDarko <lycandarko@outlook.com>
2023-06-10 13:02:05 -0700

    Cleaned up useless lines and removed overly verbose comments.

--- rabbithole/services/tag-preview.lua
+++ rabbithole/services/tag-preview.lua
@@ -11,15 +11,15 @@ local visible = false
 function TagPreview.new()
     local self = setmetatable({}, TagPreview)
     bling.widget.tag_preview.enable {
-        show_client_content = true,  -- Whether or not to show the client content
-        scale = 0.25,  -- The scale of the previews compared to the screen
-        placement_fn = function(obj)    -- Place the widget using awful.placement (this overrides x & y)
+        show_client_content = true,
+        scale = 0.25,
+        placement_fn = function(obj)
             local mouse_coords = mouse.coords()
             obj.x = mouse_coords.x - obj.width / 2
-            obj.y = mouse_coords.y + dpi(15) -- Move the object 20 pixels below the cursor
+            obj.y = mouse_coords.y + dpi(15)
             return obj
         end,
-        background_widget = wibox.widget {    -- Set a background image (like a wallpaper) for the widget 
+        background_widget = wibox.widget {
             image = beautiful.wallpaper,
             horizontal_fit_policy = "fit",
             vertical_fit_policy   = "fit",
@@ -28,26 +28,19 @@ function TagPreview.new()
     }
     return self
 end
-
-function TagPreview:show(tag, screen)
-    -- BLING: Only show widget when there are clients in the tag
+ function TagPreview:show(tag, screen)
     if #tag:clients() > 0 and not visible then
-        -- BLING: Update the widget with the new tag
         awesome.emit_signal("bling::tag_preview::update", tag)
-        -- BLING: Show the widget
         awesome.emit_signal("bling::tag_preview::visibility", screen, true)
         visible = true
     end
 end
-
-function TagPreview:hide(screen)
-    -- BLING: Turn the widget off
+ function TagPreview:hide(screen)
     if visible then
         awesome.emit_signal("bling::tag_preview::visibility", screen, false)
         visible = false
     end
 end
-
-return setmetatable(TagPreview, {
+ return setmetatable(TagPreview, {
     __call = TagPreview.new,
 })
