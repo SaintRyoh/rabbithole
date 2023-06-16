b4b06bd6d367f739cd4d949307901b215aa29b28
LycanDarko <lycandarko@outlook.com>
2023-06-16 16:05:09 -0700

    Even more robust installer. Now supports Void Linux.
    This should make the installer for Rabbithole work with virtually any Linux system.

--- rabid-installer.sh
+++ rabid-installer.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 # rabid-installer.sh By The Rabbithole Project
 # This script must be run with root privelages.
-# This script should work with any Linux distribution that uses apt, pacman or 
-# emerge as a package manager.
+# This script should work with any Linux distribution that uses apt, pacman
+# xpbs-install or emerge as a package manager. (Arch, Debian, Ubuntu, Gentoo, Void, etc.)
 
 # Set the project directory
 PROJECT_DIR="$(dirname "$(readlink -f "$0")")"
 
 # Check which package manager is installed
-
 if command -v apt >/dev/null 2>&1; then
     PACKAGE_MANAGER="apt"
 elif command -v pacman >/dev/null 2>&1; then
     PACKAGE_MANAGER="pacman"
 elif command -v emerge >/dev/null 2>&1; then
     PACKAGE_MANAGER="emerge"
+elif command -v xbps-install >/dev/null 2>&1; then
+    PACKAGE_MANAGER="xbps"
 else
     echo "No recognized package manager is available on your system. Exiting."
     exit 1
@@ -30,6 +31,8 @@ check_and_install() {
             sudo pacman -S "$1" --noconfirm
         elif [ $PACKAGE_MANAGER == "emerge" ]; then
             sudo emerge --ask "$1"
+        elif [ $PACKAGE_MANAGER == "xbps" ]; then
+            sudo xbps-install -Sy "$1"
         fi
     fi
 }
