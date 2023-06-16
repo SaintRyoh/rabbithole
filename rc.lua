84c0f8f2c63794e127a42fd66d2cdf79657f0f39
LycanDarko <lycandarko@outlook.com>
2023-06-16 02:33:47 -0700

    Cleaned up rc.lua a little bit

--- rc.lua
+++ rc.lua
@@ -11,22 +11,22 @@ require("awful.hotkeys_popup.keys")
 
 -- global namespace, on top before require any modules
 RC = {
-    diModule = require("sub.lua-di.lua-di.DependencyInjectionModule")(function (config) 
+    diModule = require("sub.lua-di.lua-di.DependencyInjectionModule")(function(config)
 
         -- Make workspaceManagerService a singleton
         config.bindings.types.workspaceManagerService = "workspaceManagerService"
         config.singletons.workspaceManagerService = true
         config.providers.workspaceManagerService = function()
-            return RC.diModule.getInstance("rabbithole.services.workspaceManagerService") 
+            return RC.diModule.getInstance("rabbithole.services.workspaceManagerService")
         end
-        
+
         config.bindings.values.settings = require("settings")
         config.bindings.types.theme = "rabbithole.services.theme-loader"
         config.singletons.theme = true -- change theme from a singleton when we implement a live theme-switcher
 
         config.enableAutoConfiguration()
 
-    end),
-} 
+    end)
+}
 
 RC.environment = RC.diModule.getInstance("rabbithole.environment")
