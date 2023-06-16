3a1544b5fc7596ae738b76b6420dfe4473463d96
LycanDarko <lycandarko@outlook.com>
2023-06-15 17:22:45 -0700

    QuickFix: Comments.

--- rabbithole/services/settingsManager.lua
+++ rabbithole/services/settingsManager.lua
@@ -1,6 +1,5 @@
 local config_dir = require("gears.filesystem").get_configuration_dir()
 
--- Create the Settings object so it's seen by the dependency injector
 local Settings = { }
 Settings.__index = Settings
 
