ff4cfa1346592eee8844d9bca2f2e2f6ed3e86b5
LycanDarko <lycandarko@outlook.com>
2023-06-15 17:21:33 -0700

    Updated settings manager so the settings file path works outside  xephyr
    Settings service loaded settings properly from inside xephyr, but the path was incorrect when actually booting Rabbithole (relative path error).

--- rabbithole/services/settingsManager.lua
+++ rabbithole/services/settingsManager.lua
@@ -1,3 +1,5 @@
+local config_dir = require("gears.filesystem").get_configuration_dir()
+
 -- Create the Settings object so it's seen by the dependency injector
 local Settings = { }
 Settings.__index = Settings
@@ -6,7 +8,7 @@ function Settings.new()
     local self = setmetatable({}, Settings)
 
     -- Load the user's settings
-    self.settings = dofile("settings.lua")
+    self.settings = dofile(config_dir .. "settings.lua")
 
     return self
 end
