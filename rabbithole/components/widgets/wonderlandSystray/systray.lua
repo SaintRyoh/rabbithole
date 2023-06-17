3f6ae86e624eb1c3674afed84a142f0e90b5fc80
SaintRyoh <14931827+SaintRyoh@users.noreply.github.com>
2023-06-17 15:43:29 -0500

    systray background

--- rabbithole/components/widgets/wonderlandSystray/systray.lua
+++ rabbithole/components/widgets/wonderlandSystray/systray.lua
@@ -10,10 +10,13 @@ local systrayContainer = wibox.widget {
       systrayWidget,
       margins = dpi(5),
       widget = wibox.container.margin,
+      -- widget = wibox.widget.textbox,
+      -- text = "systray",
   },
   shape = gears.shape.rounded_rect, -- add rounded corners to the widget
   shape_clip = true, -- clip the widget according to the shape
   widget = wibox.container.background,
+  bg = beautiful.neutral,
 }
 
 return systrayContainer
