cb990121e448b0f617f91c3c962930eb910a2126
LycanDarko <lycandarko@outlook.com>
2023-06-17 14:24:57 -0700

    BugFix: Consistent systray and tasklist container size and icons.

--- rabbithole/components/widgets/wonderlandSystray/systray.lua
+++ rabbithole/components/widgets/wonderlandSystray/systray.lua
@@ -3,20 +3,20 @@ local beautiful = require("beautiful")
 local dpi = beautiful.xresources.apply_dpi
 local gears = require("gears")
 
-local systrayWidget = wibox.widget.systray()
-
 local systrayContainer = wibox.widget {
-  {
-      systrayWidget,
-      margins = dpi(5),
-      widget = wibox.container.margin,
-      -- widget = wibox.widget.textbox,
-      -- text = "systray",
-  },
-  shape = gears.shape.rounded_rect, -- add rounded corners to the widget
-  shape_clip = true, -- clip the widget according to the shape
-  widget = wibox.container.background,
-  bg = beautiful.neutral,
+    {
+        {
+            wibox.widget.systray(),
+            margins = dpi(1.5),
+            widget = wibox.container.margin,
+        },
+        shape = gears.shape.rounded_rect, 
+        shape_clip = true,
+        widget = wibox.container.background,
+        bg = beautiful.neutral,
+    },
+    margins = dpi(4),
+    widget = wibox.container.margin
 }
 
 return systrayContainer
