bbfd378ef3b7459f66d915f3c414ca8920364527
SaintRyoh <14931827+SaintRyoh@users.noreply.github.com>
2023-06-17 18:04:42 -0500

    convert env to use table.new format

--- rabbithole/environment.lua
+++ rabbithole/environment.lua
@@ -1,10 +1,16 @@
 -- Environments are made out of systems and UIs
-return setmetatable({}, {
-    __constructor = function(
-       rabbithole__ui__default, 
-       rabbithole__services__global,
-       rabbithole__services__sloppy___focus
-    )
-        return
-    end,
-})
+
+local Environment = {}
+Environment.__index = Environment
+
+function Environment.new(
+    rabbithole__ui__default, 
+    rabbithole__services__global,
+    rabbithole__services__sloppy___focus
+)
+    local self = setmetatable({}, Environment)
+
+    return self
+end
+
+return Environment
