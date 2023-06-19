33b4c55bc96be32548a62eb5428fe9e10c303341
LycanDarko <lycandarko@outlook.com>
2023-06-18 23:49:03 -0700

    Added blinking animation to animation.lua

--- rabbithole/services/animation.lua
+++ rabbithole/services/animation.lua
@@ -1,12 +1,11 @@
 local gears = require("gears")
+local rubato = require("sub.rubato")
 
 local AnimationService = {}
 AnimationService.__index = AnimationService
 
 RUBATO_DIR = "sub.rubato."
 RUBATO_MANAGER = require("sub.rubato.manager")
-
-local rubato = require("sub.rubato")
 
 function AnimationService.new(settings)
     local self = setmetatable({}, AnimationService)
@@ -18,5 +17,17 @@ function AnimationService.new(settings)
     end
 end
 
+function AnimationService:blink(args)
+    local default_args = {
+        period = 2, -- 2 seconds for a full period
+        amplitude = 1, -- it will go from 0 to 1
+        autostart = false -- manually start it
+    }
+    
+    -- Merge default args and given args
+    local complete_args = gears.table.crush(default_args, args)
+
+    return rubato.timed_sinusoidal(complete_args)
+end
 
 return AnimationService
