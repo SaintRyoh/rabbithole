3887e2f33d533e5e1ad426ccac2f6252740be85f
LycanDarko <lycandarko@outlook.com>
2023-06-25 00:39:08 -0700

    Implemented lodash library in autostart applications.
    Removed for loops to exercise more functional programming.

--- rabbithole/services/global.lua
+++ rabbithole/services/global.lua
@@ -1,5 +1,6 @@
 local awful = require("awful")
 local gears = require("gears")
+local __ = require("lodash")
 
 return setmetatable({}, {
     __constructor = function (
@@ -19,15 +20,22 @@ return setmetatable({}, {
         awful.layout.layouts = rabbithole__components__layouts__default
 
         -- Set default client rules
-        -- awful.rules.rules = rabbithole__components__rules__client___default
         awful.rules.rules = gears.table.join(
             awful.rules.rules,
             rabbithole__components__rules__client___default
         )
 
-        -- Spawn clients in the autostart settings
-        for _, app in ipairs(settings.autostart_apps) do
-            awful.spawn.with_shell(app)
+        -- Spawn clients in the daemons settings
+        if settings.daemons then
+            __.forEach(settings.daemons, function(daemon)
+                awful.spawn.with_shell(daemon)
+            end)
+        end
+        -- Autostart applications
+        if settings.autostart_apps then
+            __.forEach(settings.autostart_apps, function(app)
+                awful.spawn.with_shell(app)
+            end)
         end
     end
 })
