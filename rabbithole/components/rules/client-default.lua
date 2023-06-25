121f2850553097869432fcfcbcd8fa15e76088fc
LycanDarko <lycandarko@outlook.com>
2023-06-25 00:19:42 -0700

    Removed useless floating client rules

--- rabbithole/components/rules/client-default.lua
+++ rabbithole/components/rules/client-default.lua
@@ -18,28 +18,28 @@ return setmetatable({}, {
                 placement = awful.placement.no_overlap + awful.placement.no_offscreen
             }
         }, -- Floating clients.
-        {
-            rule_any = {
-                instance = {"DTA", -- Firefox addon DownThemAll.
-                "copyq", -- Includes session name in class.
-                "pinentry"},
-                class = {"Arandr", "Blueman-manager", "Gpick", "Kruler", "MessageWin", -- kalarm.
-                "Sxiv", "Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
-                "Wpa_gui", "veromix", "xtightvncviewer"},
-
-                -- Note that the name property shown in xprop might be set slightly after creation of the client
-                -- and the name shown there might not match defined rules here.
-                name = {"Event Tester" -- xev.
-                },
-                role = {"AlarmWindow", -- Thunderbird's calendar.
-                "ConfigManager", -- Thunderbird's about:config.
-                "pop-up" -- e.g. Google Chrome's (detached) Developer Tools.
-                }
-            },
-            properties = {
-                floating = true
-            }
-        }, -- Add titlebars to normal clients and dialogs
+        --{
+        --    rule_any = {
+        --        instance = {"DTA", -- Firefox addon DownThemAll.
+        --        "copyq", -- Includes session name in class.
+        --        "pinentry"},
+        --        class = {"Arandr", "Blueman-manager", "Gpick", "Kruler", "MessageWin", -- kalarm.
+        --        "Sxiv", "Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
+        --        "Wpa_gui", "veromix", "xtightvncviewer"},
+--
+        --        -- Note that the name property shown in xprop might be set slightly after creation of the client
+        --        -- and the name shown there might not match defined rules here.
+        --        name = {"Event Tester" -- xev.
+        --        },
+        --        role = {"AlarmWindow", -- Thunderbird's calendar.
+        --        "ConfigManager", -- Thunderbird's about:config.
+        --        "pop-up" -- e.g. Google Chrome's (detached) Developer Tools.
+        --        }
+        --    },
+        --    properties = {
+        --        floating = true
+        --    }
+        --}, -- Add titlebars to normal clients and dialogs
         {
             rule_any = {
                 type = {"normal", "dialog"}
