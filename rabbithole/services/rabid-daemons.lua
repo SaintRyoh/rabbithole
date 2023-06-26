20b1ba8c9f2aa1e9991c718b1e891cbcebe207e6
LycanDarko <lycandarko@outlook.com>
2023-06-25 19:17:42 -0700

    rabid-daemons completed plugged in

--- rabbithole/services/rabid-daemons.lua
+++ rabbithole/services/rabid-daemons.lua
@@ -5,45 +5,52 @@ applications. It is called from rabbithole/services/global.lua.
 This ensures that the daemons and applications are started after everything 
 else and that they are started only once.
 ]]
-local awful require("awful")
+local awful = require("awful")
 local __ = require("lodash")
 
 
-return setmetatable({ }, {
-    __constructor = function (
-        settings
-    )
-        -- Run a command only if it is not already running
-        local function run_once(cmd)
-            local findme = cmd
-            local firstspace = cmd:find(" ")
-            if firstspace then
-                findme = cmd:sub(0, firstspace-1)
-            end
-            -- Run pgrep, capture the output into data
-            awful.spawn.with_line_callback("pgrep -u $USER -x " .. findme,
-                {
-                    stdout = function(line)
-                        if line == '' then
-                            awful.spawn.with_shell(cmd)
-                        end
-                    end
-                }
-            )
-        end
+local RabidDaemons = { }
+RabidDaemons.__index = RabidDaemons
 
-        -- Spawn clients in the daemons settings
-        if settings.daemons then
-            __.forEach(settings.daemons, function(daemon)
-                run_once(daemon)
-            end)
-        end
+function RabidDaemons.new(settings)
+    local self = setmetatable({}, RabidDaemons)
 
-        -- Autostart applications
-        if settings.autostart_apps then
-            __.forEach(settings.autostart_apps, function(app)
-                run_once(app)
-            end)
-        end
+    self.settings = settings
+
+    return self
+end
+
+function RabidDaemons:run_once(cmd)
+    local findme = cmd
+    local firstspace = cmd:find(" ")
+    if firstspace then
+        findme = cmd:sub(0, firstspace-1)
     end
-})
+    -- Run pgrep, capture the output into data
+    awful.spawn.with_line_callback("pgrep -u $USER -x " .. findme,
+        {
+            stdout = function(line)
+                if line == '' then
+                    awful.spawn.with_shell(cmd)
+                end
+            end
+        }
+    )
+end
+
+function RabidDaemons:run()
+    -- Spawn clients in the daemons settings
+    if self.settings.daemons then
+        __.forEach(self.settings.daemons, function(daemon)
+            self:run_once(daemon)
+        end)
+    end
+    -- Autostart applications
+    if self.settings.autostart_apps then
+        __.forEach(self.settings.autostart_apps, function(app)
+            self:run_once(app)
+        end)
+    end
+end
+
+return RabidDaemons
