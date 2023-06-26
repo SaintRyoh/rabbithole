31500d916fbc77be4bc202e55c799c6973535270
LycanDarko <lycandarko@outlook.com>
2023-06-25 21:37:00 -0700

    attempt 3 on new installer

--- rabid-installer.sh
+++ rabid-installer.sh
@@ -3,7 +3,6 @@
 
 # This script must be run with root privileges.
 # This script should work with any Linux distribution that uses apt, pacman, xbps-install or emerge as a package manager. (Arch, Debian, Ubuntu, Gentoo, Void, etc.)
-
 # Core dependencies
 CORE_DEPENDENCIES=(
   awesome
@@ -26,7 +25,7 @@ DE_LIKE_DEPENDENCIES=(
 )
 
 # Set the project directory
-PROJECT_DIR="$(dirname "$(readlink -f "$0")")"
+PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Check which package manager is installed
 if command -v apt >/dev/null 2>&1; then
@@ -47,13 +46,13 @@ fi
 
 check_and_install() {
     if [ $PACKAGE_MANAGER == "apt" ]; then
-        sudo apt install "$1" -y
+        dpkg -s "$1" >/dev/null 2>&1 || sudo apt install "$1" -y
     elif [ $PACKAGE_MANAGER == "pacman" ] || [ $PACKAGE_MANAGER == "yay" ]; then
-        sudo $PACKAGE_MANAGER -S "$1" --noconfirm
+        pacman -Q "$1" >/dev/null 2>&1 || $PACKAGE_MANAGER -S "$1"
     elif [ $PACKAGE_MANAGER == "emerge" ]; then
-        sudo emerge --ask "$1"
+        emerge -pq "$1" >/dev/null 2>&1 || sudo emerge --ask "$1"
     elif [ $PACKAGE_MANAGER == "xbps" ]; then
-        sudo xbps-install -Sy "$1"
+        xbps-query -R "$1" >/dev/null 2>&1 || sudo xbps-install -Sy "$1"
     fi
 }
 
@@ -63,30 +62,17 @@ echo "1. Core (For Advanced Users)"
 echo "2. DE-Like (Recommended)"
 read -p "Enter your choice (1-2): " INSTALL_TYPE
 
-# Check if a package is installed
-check_if_installed() {
-    if [ $PACKAGE_MANAGER == "apt" ]; then
-        dpkg -s "$1" >/dev/null 2>&1 && echo "Package $1 is already installed" || check_and_install "$1"
-    elif [ $PACKAGE_MANAGER == "pacman" ] || [ $PACKAGE_MANAGER == "yay" ]; then
-        pacman -Q "$1" >/dev/null 2>&1 && echo "Package $1 is already installed" || check_and_install "$1"
-    elif [ $PACKAGE_MANAGER == "emerge" ]; then
-        emerge -pq "$1" >/dev/null 2>&1 && echo "Package $1 is already installed" || check_and_install "$1"
-    elif [ $PACKAGE_MANAGER == "xbps" ]; then
-        xbps-query -R "$1" >/dev/null 2>&1 && echo "Package $1 is already installed" || check_and_install "$1"
-    fi
-}
-
 # Use check_if_installed instead of check_and_install in the script
 if [ "$INSTALL_TYPE" = "1" ]; then
   for pkg in "${CORE_DEPENDENCIES[@]}"; do
-      check_if_installed "$pkg"
+      check_and_install "$pkg"
   done
 elif [ "$INSTALL_TYPE" = "2" ]; then
   for pkg in "${CORE_DEPENDENCIES[@]}"; do
-      check_if_installed "$pkg"
+      check_and_install "$pkg"
   done
   for pkg in "${DE_LIKE_DEPENDENCIES[@]}"; do
-      check_if_installed "$pkg"
+      check_and_install "$pkg"
   done
 fi
 
@@ -128,21 +114,14 @@ if [ $PACKAGE_MANAGER != "yay" ]; then
     fi
 fi
 
-# Check and install Awesome WM, Picom, Rofi, and Git
-check_and_install awesome
-check_and_install picom
-check_and_install rofi
-check_and_install git
-
 if [ ! -d "$HOME/.config/awesome" ]; then
     mkdir -p "$HOME/.config/awesome"
 fi
-cp -R "$PROJECT_DIR/rabbithole/configs/." "$HOME/.config/awesome/"
+cp -R "$PROJECT_DIR/configs/." "$HOME/.config/awesome/"
 
 if [ ! -d "$HOME/.config/picom" ]; then
     mkdir -p "$HOME/.config/picom"
 fi
-cp "$PROJECT_DIR/picom.conf" "$HOME/.config/picom/picom.conf"
 
 # Install autorandr to /usr/bin/
 sudo cp "$PROJECT_DIR/installer/autorandr" /usr/bin/
