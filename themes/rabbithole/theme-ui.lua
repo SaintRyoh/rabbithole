9e1440fad856a69f1a7d13604469a59aa5d559cf
LycanDarko <lycandarko@outlook.com>
2023-07-01 20:28:45 -0700

    Added variables for wsm, menu, taglist, & tasklist.

--- themes/rabbithole/theme-ui.lua
+++ themes/rabbithole/theme-ui.lua
@@ -3,16 +3,19 @@ local dpi = require("beautiful.xresources").apply_dpi
 
 local theme_ui = {}
 
-
--- Misc UI
+-- Window frame UI
 theme_ui.useless_gap = dpi(2)
 theme_ui.border_width = dpi(3)
--- menu
+-- Standard menu options
 theme_ui.menu_height = dpi(15)
 theme_ui.menu_width = dpi(120)
+-- Workspace menu
+theme_ui.ws_menu_height = dpi(34)
+theme_ui.ws_menu_width = dpi(120)
 -- Tag options
 theme_ui.taglist_shape = gears.shape.rounded_rect
 theme_ui.taglist_spacing = 1
+
 -- Tasklist options
 theme_ui.tasklist_shape = gears.shape.rounded_rect
 theme_ui.tasklist_shape_border_width = dpi(1)
