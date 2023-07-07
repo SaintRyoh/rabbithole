826bae67e4bfd5544e4fb903ab638e01d379a1cf
LycanDarko <lycandarko@outlook.com>
2023-07-07 14:27:43 -0700

    reverted global.lua

--- rabbithole/components/buttons/global.lua
+++ rabbithole/components/buttons/global.lua
@@ -14,21 +14,6 @@ return setmetatable({}, {
                 --awful.button({ }, 4, awful.tag.viewnext),
                 --awful.button({ }, 5, awful.tag.viewprev)
         )
-            -- When a client is clicked with the right mouse button, activate dragging
---            client.connect_signal("button::press", function(c)
---                if awful.mouse.client_under_pointer() == c then
---                    client.focus = c
---                    awful.mouse.client.move(c)
---                end
---            end)
---
---            -- When the mouse button is released, move the client to the tag under the mouse
---            client.connect_signal("button::release", function(c)
---                local t = awful.screen.focused().selected_tag
---                if t then
---                    c:move_to_tag(t)
---                end
---            end)
 
         return globalbuttons
     end
