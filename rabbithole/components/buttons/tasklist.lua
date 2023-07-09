bfb2bb6eb0d857dd9e7f8c8bea757b404e7f0592
LycanDarko <lycandarko@outlook.com>
2023-07-08 20:33:56 -0700

    bringing back minimize feature

--- rabbithole/components/buttons/tasklist.lua
+++ rabbithole/components/buttons/tasklist.lua
@@ -1,21 +1,21 @@
 local awful = require("awful")
 local gears = require("gears")
-local dragndrop = require("rabbithole.services.dragondrop")
 
-return setmetatable({ }, {
+return setmetatable({}, {
     __constructor = function (
         rabbithole__components__menus__tasklist
     )
         local tasklist_buttons = gears.table.join(
                 awful.button({ }, 1, function (c)
-                    --if c.minimized then
-                    --    c.minimized = false
-                    --else
-                    --    c:emit_signal("request::activate", "tasklist", {raise = true})
-                    --end
-                    --dragndrop:drag(c)
-                    --c:connect_signal("mouse::pressed", function() dragndrop:drag(c) end)
-                    --c:connect_signal("mouse::released", function() dragndrop:drop() end)
+                    if c == client.focus then
+                        c.minimized = true
+                    else
+                        c:emit_signal(
+                                "request::activate",
+                                "tasklist",
+                                {raise = true}
+                        )
+                    end
                 end),
                 -- middle click to kill client
                 awful.button({ }, 2, function(c)
