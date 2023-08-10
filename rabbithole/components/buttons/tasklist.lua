c3695d0b7d2f49f51302cb023ac20c1c4cf75ca1
LycanDarko <lycandarko@outlook.com>
2023-08-09 19:02:00 -0700

    Removed redundant logic from tasklist since its in dragondrop

--- rabbithole/components/buttons/tasklist.lua
+++ rabbithole/components/buttons/tasklist.lua
@@ -6,17 +6,18 @@ return setmetatable({}, {
         rabbithole__components__menus__tasklist
     )
         local tasklist_buttons = gears.table.join(
-                awful.button({ }, 1, function (c)
-                    if c == client.focus then
-                        c.minimized = true
-                    else
-                        c:emit_signal(
-                                "request::activate",
-                                "tasklist",
-                                {raise = true}
-                        )
-                    end
-                end),
+                -- NOTE: i DONT THINK WE NEED THIS LOGIC ANYMORE BECAUSE OF THE DRAGONDROP MODULE LOGIC
+                --awful.button({ }, 1, function (c)
+                --    if c == client.focus then
+                --        c.minimized = true
+                --    else
+                --        c:emit_signal(
+                --                "request::activate",
+                --                "tasklist",
+                --                {raise = true}
+                --        )
+                --    end
+                --end),
                 -- middle click to kill client
                 awful.button({ }, 2, function(c)
                     c:kill()
