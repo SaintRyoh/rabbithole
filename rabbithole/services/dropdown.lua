6ec0d8596565a4a9e065cceda162cd0fbde066b6
LycanDarko <lycandarko@outlook.com>
2023-09-20 18:05:24 -0700

    Refeactored dropdown. Really clean it up

--- rabbithole/services/dropdown.lua
+++ rabbithole/services/dropdown.lua
@@ -1,15 +1,9 @@
---[[
-
-     Licensed under GNU General Public License v2
-      * (c) 2016, Luca CPZ
-      * (c) 2015, unknown
-
---]]
 local awful = require("awful")
 local gears = require("gears")
 
 local capi = {client = client}
 
+-- Easing function for animation, remove when animation library is brought back fron the dead
 local function outCubic(t, b, c, d)
     t = t / d - 1
     return c * (t * t * t + 1) + b
@@ -18,30 +12,25 @@ end
 -- Dropdown application container widget
 local Dropdown = {}
 
--- If you have a rule like "awful.client.setslave" for your terminals,
--- ensure you use an exception for QuakeDD. Otherwise, you may
--- run into problems with focus.
-
 function Dropdown:new(config)
     local conf = config or {}
-
-    conf.app = conf.app or "xterm" -- application to spawn
-    conf.name = conf.name or "Dropdown" -- window name
-    conf.argname = conf.argname or "-name %s" -- how to specify window name
-    conf.extra = conf.extra or "" -- extra arguments
-    conf.border = conf.border or 1 -- client border width
-    conf.visible = conf.visible or false -- initially not visible
-    conf.followtag = conf.followtag or false -- spawn on currently focused screen
-    conf.overlap = conf.overlap or false -- overlap wibox
-    conf.screen = conf.screen or awful.screen.focused()
-    conf.settings = conf.settings
-
-    -- If width or height <= 1 this is a proportion of the workspace
-    conf.height = conf.height or 0.25 -- height
-    conf.width = conf.width or 0.75 -- width
-    conf.vert = conf.vert or "top" -- top, bottom or center
-    conf.horiz = conf.horiz or "center" -- left, right or center
-    conf.geometry = {} -- internal use
+    conf = {
+        app = conf.app or "xterm",
+        name = conf.name or "Dropdown",
+        argname = conf.argname or "-name %s",
+        extra = conf.extra or "",
+        border = conf.border or 1,
+        visible = conf.visible or false,
+        followtag = conf.followtag or false,
+        overlap = conf.overlap or false,
+        screen = conf.screen or awful.screen.focused(),
+        settings = conf.settings,
+        height = conf.height or 0.25,
+        width = conf.width or 0.75,
+        vert = conf.vert or "top",
+        horiz = conf.horiz or "center",
+        geometry = {}
+    }
 
     local dropdown = setmetatable(conf, { __index = Dropdown })
 
@@ -50,6 +39,7 @@ function Dropdown:new(config)
             dropdown:display()
         end
     end)
+
     capi.client.connect_signal("unmanage", function(c)
         if c.instance == dropdown.name and c.screen == dropdown.screen then
             dropdown.visible = false
@@ -59,87 +49,52 @@ function Dropdown:new(config)
     return dropdown
 end
 
+-- Dropdown Management
 function Dropdown:display()
     if self.followtag then
         self.screen = awful.screen.focused()
     end
 
-    -- First, we locate the client
-    local client = nil
-    local i = 0
-    for c in awful.client.iterate(function(c)
-        -- c.name may be changed!
+    local client = awful.client.iterate(function(c)
         return c.instance == self.name
-    end) do
-        i = i + 1
-        if i == 1 then
-            client = c
-        else
-            -- Additional matching clients, let's remove the sticky bit
-            -- which may persist between awesome restarts. We don't close
-            -- them as they may be valuable. They will just turn into
-            -- normal clients.
-            c.sticky = false
-            c.ontop = false
-            c.above = false
-        end
-    end
+    end)()
 
     if not client and not self.visible then
         return
     end
 
     if not client then
-        -- The client does not exist, we spawn it
-        local cmd = string.format("%s %s %s", self.app, string.format(self.argname, self.name), self.extra)
-        awful.spawn(cmd, {
+        awful.spawn(string.format("%s %s %s", self.app, string.format(self.argname, self.name), self.extra), {
             tag = self.screen.selected_tag
         })
         return
     end
 
-    -- Set geometry
     client.floating = true
     client.border_width = self.border
     client.size_hints_honor = false
     client:geometry(self.geometry[self.screen.index] or self:compute_size())
-
-    -- Set not sticky and on top
     client.sticky = false
     client.ontop = true
     client.above = true
     client.skip_taskbar = true
-
-    -- Additional user settings
     if self.settings then
         self.settings(client)
     end
 
-    -- Set the initial geometry
+    -- Animate client height
     local target_geometry = self.geometry[self.screen.index] or self:compute_size()
+    local anim_target = self.visible and target_geometry.height or 0
+    local anim_duration = 0.3
+    local anim_steps = 30
+    local current_step = 0
 
-    -- Extract repetitive animation logic into a separate function
-        local function animateHeight(target_height)
-            local anim_duration = 0.3
-            local anim_steps = 30
-            local step_delay = anim_duration / anim_steps
-    
-            local current_step = 0
-            gears.timer.start_new(step_delay, function()
-                local new_height = outCubic(current_step, 1, target_height - 1, anim_steps)
-                client:geometry({ height = math.ceil(new_height) })
-                current_step = current_step + 1
-                return current_step <= anim_steps
-            end)
-        end
-    
-        if not self.visible then
-            -- Hiding animation
-            animateHeight(0)
-        else
-            -- Showing animation
-            animateHeight(target_geometry.height)
-        end
+    gears.timer.start_new(anim_duration / anim_steps, function()
+        local new_height = outCubic(current_step, 1, anim_target - 1, anim_steps)
+        client:geometry({ height = math.ceil(new_height) })
+        current_step = current_step + 1
+        return current_step <= anim_steps
+    end)
 
     if self.visible then
         client.hidden = false
@@ -149,47 +104,22 @@ function Dropdown:display()
         capi.client.focus = client
     else
         client.hidden = true
-        local ctags = client:tags()
-        for i, t in pairs(ctags) do
-            ctags[i] = nil
-        end
-        client:tags(ctags)
+        client:tags({})
     end
 
     return client
 end
 
 function Dropdown:compute_size()
-    -- skip if we already have a geometry for this screen
     if not self.geometry[self.screen.index] then
-        local geom
-        if not self.overlap then
-            geom = screen[self.screen.index].workarea
-        else
-            geom = screen[self.screen.index].geometry
-        end
-        local width, height = self.width, self.height
-        if width <= 1 then
-            width = math.floor(geom.width * width) - 2 * self.border
-        end
-        if height <= 1 then
-            height = math.floor(geom.height * height)
-        end
-        local x, y
-        if self.horiz == "left" then
-            x = geom.x
-        elseif self.horiz == "right" then
-            x = geom.width + geom.x - width
-        else
-            x = geom.x + (geom.width - width) / 2
-        end
-        if self.vert == "top" then
-            y = geom.y
-        elseif self.vert == "bottom" then
-            y = geom.height + geom.y - height
-        else
-            y = geom.y + (geom.height - height) / 2
-        end
+        local geom = self.overlap and screen[self.screen.index].geometry or screen[self.screen.index].workarea
+
+        local width = (self.width <= 1) and math.floor(geom.width * self.width) - 2 * self.border or self.width
+        local height = (self.height <= 1) and math.floor(geom.height * self.height) or self.height
+
+        local x = (self.horiz == "left") and geom.x or (self.horiz == "right") and geom.width + geom.x - width or geom.x + (geom.width - width) / 2
+        local y = (self.vert == "top") and geom.y or (self.vert == "bottom") and geom.height + geom.y - height or geom.y + (geom.height - height) / 2
+
         self.geometry[self.screen.index] = {
             x = x,
             y = y,
