3646b7dfc5f850c302b08c8a4044158d0dad689d
LycanDarko <lycandarko@outlook.com>
2023-10-03 02:07:35 -0700

    fixed bug if screen resolution cant be found

--- rabbithole/services/detect-monitors.lua
+++ rabbithole/services/detect-monitors.lua
@@ -13,7 +13,6 @@ local gears = require("gears")
 local DetectMonitor = {}
 DetectMonitor.__index = DetectMonitor
 
-
 function DetectMonitor.new()
     return setmetatable({}, DetectMonitor)
 end
@@ -24,14 +23,20 @@ function DetectMonitor.setup_screens()
         for _, line in pairs(screens) do
             local screen_name = line:match("^(.-) connected")
             if screen_name then
-                -- finds and sets the highest available resolution for the connected screen
+                -- Find and set the highest available resolution for the connected screen
                 local max_res = line:match("%d+x%d+")
-                awful.spawn.easy_async_with_shell("xrandr --output " .. screen_name .. " --mode " .. max_res, function()
-                    -- save the screen config w/ autorandr
-                    awful.spawn.easy_async_with_shell("autorandr --save " .. screen_name, function()
-                        naughty.notify({ title = "Monitor Configuration", text = "Applied and saved configuration for " .. screen_name })
+                if max_res then
+                    awful.spawn.easy_async_with_shell("xrandr --output " .. screen_name .. " --mode " .. max_res, function()
+                        -- save users setup for the future
+                        awful.spawn.easy_async_with_shell("autorandr --save " .. screen_name, function()
+                            naughty.notify({ title = "Automatic Display Configuration", text = "Applied and saved configuration for " .. screen_name })
+                        end)
                     end)
-                end)
+                else  -- Handle the case where max_res could not be determined
+                    awful.spawn.easy_async_with_shell("autorandr --change", function()
+                        naughty.notify({ title = "Automatic Display Configuration", text = "Maximum resolution could not be determined for " .. screen_name .. ". Autodetected settings." })
+                    end)
+                end
             end
         end
     end)
@@ -59,3 +64,4 @@ awful.spawn.with_line_callback("stdbuf -oL udevadm monitor --property --subsyste
 })
 
 return DetectMonitor
+
