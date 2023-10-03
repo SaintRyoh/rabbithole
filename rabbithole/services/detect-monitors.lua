f14d9721bf9852f094c57d6bd8de7f7099c25a6f
LycanDarko <lycandarko@outlook.com>
2023-10-03 11:10:19 -0700

    fixed?

--- rabbithole/services/detect-monitors.lua
+++ rabbithole/services/detect-monitors.lua
@@ -18,37 +18,28 @@ function DetectMonitor.new()
 end
 
 function DetectMonitor.setup_screens()
-    awful.spawn.easy_async_with_shell("xrandr -q", function(stdout, stderr, exitreason, exitcode)
-        if exitcode ~= 0 then
-            naughty.notify({ title = "Error Querying xrandr", text = stderr, preset = naughty.config.presets.critical })
-            return
-        end
-        
-        local connected_screens = {}
-        for line in stdout:gmatch("[^\r\n]+") do
+    awful.spawn.easy_async_with_shell("xrandr -q | grep ' connected'", function(stdout)
+        local screens = gears.string.split(stdout, "\n")
+        for _, line in pairs(screens) do
             local screen_name = line:match("^(.-) connected")
             if screen_name then
-                connected_screens[#connected_screens + 1] = {name=screen_name, xrandr_output=line}
-            end
-        end
-        
-        for _, screen_info in ipairs(connected_screens) do
-            local max_res = screen_info.xrandr_output:match(" (%d+x%d+) %s+%d+.%d%d?%*?")
-            if max_res then
-                awful.spawn.easy_async_with_shell("xrandr --output " .. screen_info.name .. " --mode " .. max_res, function()
-                    awful.spawn.easy_async_with_shell("autorandr --save " .. screen_info.name, function()
-                        naughty.notify({ title = "Monitor Configuration", text = "Applied and saved configuration for " .. screen_info.name })
+                local max_res = line:match(" (%d+x%d+)")
+                if max_res then
+                    awful.spawn.easy_async_with_shell("xrandr --output " .. screen_name .. " --mode " .. max_res, function()
+                        -- save users setup for the future
+                        awful.spawn.easy_async_with_shell("autorandr --save " .. screen_name, function()
+                            naughty.notify({ title = "Automatic Display Configuration", text = "Applied and saved configuration for " .. screen_name })
+                        end)
                     end)
-                end)
-            else
-                awful.spawn.easy_async_with_shell("autorandr --change", function()
-                    naughty.notify({ title = "Monitor Configuration", text = "Maximum resolution could not be determined for " .. screen_info.name .. ". Applied autorandr --change." })
-                end)
+                else  -- default to auto detection if display can't be determined
+                    awful.spawn.easy_async_with_shell("autorandr --change", function()
+                        naughty.notify({ title = "Automatic Display Configuration", text = "Maximum resolution could not be determined for " .. screen_name .. ". Autodetected settings applied." })
+                    end)
+                end
             end
         end
     end)
 end
-
 
 local debounce_timer = nil
 local debounce_time = 2 -- in seconds
