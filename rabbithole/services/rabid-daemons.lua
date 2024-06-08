8a871697146890e05f25caa153857d01a0d371e5
LycanDarko <lycandarko@outlook.com>
2024-06-07 22:49:04 -0700

    Rewrote rabid-daemons to be much more extensible and to run user-scripts.

--- rabbithole/services/rabid-daemons.lua
+++ rabbithole/services/rabid-daemons.lua
@@ -1,13 +1,17 @@
 --[[ rabid-daemons.lua
 
-    This file contains functions that are used to start daemons and
-applications. It is called from rabbithole/services/global.lua.
-This ensures that the daemons and applications are started after everything 
-else and that they are started only once.
+    This file contains functions that are used to start daemons and applications.
+It also is in charge of running user scripts from the rabbithole/user-scripts/ folderd.
+
+It is called from rabbithole/services/global.lua.
+
+This module ensures that daemons, applications, and scripts are started AFTER everything 
+else and that they are started only ONCE.
 ]]
 local awful = require("awful")
 local __ = require("lodash")
-
+local gears = require("gears")
+local lfs = require("lfs")
 
 local RabidDaemons = { }
 RabidDaemons.__index = RabidDaemons
@@ -20,7 +24,7 @@ function RabidDaemons.new(settings)
     return self
 end
 
-function RabidDaemons:run_once(cmd)
+function RabidDaemons:runOnce(cmd)
     -- Run pgrep, capture the output into data
     awful.spawn.easy_async_with_shell("pgrep -u $USER -f -x '" .. cmd .. "'",
         function(stdout, stderr, reason, exit_code)
@@ -33,17 +37,35 @@ function RabidDaemons:run_once(cmd)
     )
 end
 
-function RabidDaemons:run()
-    -- Spawn clients in the daemons settings
-    if self.settings.daemons then
-        __.forEach(self.settings.daemons, function(daemon)
-            self:run_once(daemon)
+function RabidDaemons:runUserScripts(scripts_dir)
+    -- Runs all .sh and .lua scripts in user-scripts directory.
+    local scripts = scripts_dir or gears.filesystem.get_configuration_dir() .. "user-scripts"
+    __.forEach(lfs.dir(scripts), function(script)
+        if script ~= "." and script ~= ".." then
+            local script_file = scripts .. "/" .. script
+            if lfs.attributes(script_file, "mode") == "file" then
+                if script_file:match("%.sh$") or script_file:match("%.lua$") then
+                    self:run_once(script_file)
+                end
+            end
+        end
+    end)
+end
+
+function RabidDaemons:startDaemons(daemons)
+    daemons = daemons or self.settings.daemons
+    if daemons then
+        __.forEach(daemons, function(daemon)
+            self:runOnce(daemon)
         end)
     end
-    -- Autostart applications
-    if self.settings.autostart_apps then
-        __.forEach(self.settings.autostart_apps, function(app)
-            self:run_once(app)
+end
+
+function RabidDaemons:startAutostartApps(apps)
+    apps = apps or self.settings.autostart_apps
+    if apps then
+        __.forEach(apps, function(app)
+            self:runOnce(app)
         end)
     end
 end
