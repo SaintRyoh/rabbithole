fcc6a2bf41b0ded353a2c47db0918402b676e90f
LycanDarko <lycandarko@outlook.com>
2024-06-28 13:22:36 -0700

    Set dropdown to alacritty again.

--- rabbithole/ui/default/init.lua
+++ rabbithole/ui/default/init.lua
@@ -30,7 +30,7 @@ function UserInterface.new(
         rabbithole__ui__default__center(s)
 
         -- create dropdown terminal box for each screen
-        s.dropdown = dropdown({app=settings.drivers.terminal, argname="--title %s", extra="--class Dropdown -e tmux", visible=true, height=0.9, screen = s })
+        s.dropdown = dropdown({app="alacritty", argname="--title %s", extra="--class Dropdown -e tmux", visible=true, height=0.75, screen = s })
         
         -- set scrreen dpi
         beautiful.xresources.set_dpi(dpi, s)
