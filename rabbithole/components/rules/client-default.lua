0a18cac582ead68f3a1d5121514d1c0fde16122e
SaintRyoh <14931827+SaintRyoh@users.noreply.github.com>
2024-06-29 20:12:05 -0500

    rule cleanup for qterminal dropdown

--- rabbithole/components/rules/client-default.lua
+++ rabbithole/components/rules/client-default.lua
@@ -3,14 +3,21 @@ local gears = require("gears")
 
 -- reading
 -- https://awesomewm.org/apidoc/libraries/awful.rules.html
+-- https://awesomewm.org/doc/api/libraries/awful.rules.html
 
-return setmetatable({}, {
-    __constructor = function(rabbithole__components__keys__client, rabbithole__components__buttons__client)
-        local rules = {
+local Rules = {}
+Rules.__index = Rules
+
+function Rules.new(
+    rabbithole__components__keys__client,
+    rabbithole__components__buttons__client
+)
+    return {
         -- All clients will match this rule.
         {
             rule = {},
             properties = {
+                titlebars_enabled = true,
                 focus = awful.client.focus.filter,
                 raise = false,
                 keys = rabbithole__components__keys__client,
@@ -21,69 +28,31 @@ return setmetatable({}, {
         },
         -- Dialogs
         {
-            rule_any = {
-                type = {"normal", "dialog"}
+            rule = {
+                type = "dialog"
             },
             properties = {
-                titlebars_enabled = true
+                titlebars_enabled = true,
+                skip_taskbar = true,
+                floating = true,
+                ontop = true
             }
-        }, -- no titlebars for launchers
+        }, 
+        -- Launchers
         {
             rule_any = {
-                class = {"dmenu", "rofi", "lxqt-runner"}
+                class = {"dmenu", "rofi", "lxqt-runner", "qterminal"},
+            },
+            except = {
+                type = "normal"
             },
             properties = {
                 titlebars_enabled = false,
                 placement = awful.placement.centered,
-                skip_taskbar = true,
-                floating = true,
-                ontop = true,
                 sticky = true
             }
         },
-        -- No titlebars for terminal (alacritty)
-        {
-            rule_any = {
-                class = {"Alacritty"}
-            },
-            properties = {
-                titlebars_enabled = false
-            }
-        },
-        -- No titlesbars for kitty terminal
-        {
-            rule_any = {
-                class = {"kitty"}
-            },
-            properties = {
-                titlebars_enabled = false
-            }
-        },
-        -- Set window corner rounding to 5px
-        {
-            rule_any = {
-                type = {"normal", "dialog"}
-            },
-            callback = function(c)
-                gears.surface.apply_shape_bounding(c, gears.shape.rounded_rect, 5)
-            end
-        }, -- Round client window borders
-        {
-            rule_any = {
-                type = {"normal", "dialog"}
-            },
-            callback = function(c)
-                if c.round_corners then
-                    return
-                end
-
-                c.shape = function(cr, width, height)
-                    gears.shape.rounded_rect(cr, width, height, 10)
-                end
-
-                c.round_corners = true
-            end
-        }, -- Clients with custom titlebars.
+        -- Clients with custom titlebars.
         {
             rule = {
                 requests_no_titlebar = true
@@ -91,7 +60,8 @@ return setmetatable({}, {
             properties = {
                 titlebars_enabled = false
             }
-        }}
-        return rules
-    end
-})
+        }
+    }
+end
+
+return Rules
