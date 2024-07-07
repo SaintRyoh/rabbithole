3aeeef90f73cd6060e27588769f45b4b02057a1d
LycanDarko <lycandarko@outlook.com>
2024-07-07 00:20:57 -0700

    Fixed default layouts

--- rabbithole/components/layouts/default.lua
+++ rabbithole/components/layouts/default.lua
@@ -4,24 +4,24 @@ return setmetatable({}, {
     __constructor = function ()
         -- TODO - pull this from settings
         return {
-            awful.layout.suit.spiral,
-            awful.layout.suit.tile,
-            awful.layout.suit.spiral.dwindle,
-            awful.layout.suit.floating,
-            awful.layout.suit.magnifier,
+            awful.layout.suit.tile,             -- 2:
+            awful.layout.suit.tile.left,        -- 3:
+            awful.layout.suit.spiral.dwindle,     -- 9:
+            awful.layout.suit.floating,           -- 1:
+            --awful.layout.suit.magnifier,          -- 12:
+            awful.layout.suit.tile.bottom,      -- 4:
             
-            -- awful.layout.suit.tile.left,
-            -- awful.layout.suit.tile.bottom,
-            -- awful.layout.suit.tile.top,
-            
-            -- awful.layout.suit.fair,
-            -- awful.layout.suit.fair.horizontal,
+            -- awful.layout.suit.tile.top,         -- 5:
 
+            -- awful.layout.suit.fair,             -- 6:
+            -- awful.layout.suit.fair.horizontal,  -- 7:
 
-            -- awful.layout.suit.max,
-            -- awful.layout.suit.max.fullscreen,
+            -- awful.layout.suit.spiral,             -- 8:
 
-            -- awful.layout.suit.corner.nw
+            -- awful.layout.suit.max,                -- 10:
+            -- awful.layout.suit.max.fullscreen,     -- 11:
+
+            -- awful.layout.suit.corner.nw           -- 13:
             --  awful.layout.suit.corner.ne,
             --  awful.layout.suit.corner.sw,
             --  awful.layout.suit.corner.se,
